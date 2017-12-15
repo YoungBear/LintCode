a71b6aa85056c2938f8fbeaeaf7ab3e7d935cd8e
youngbear <yangshaoxiong2008@163.com>
2017-12-15 15:33:33 +0800

    add computeLastDigit
    729. 阶乘除法的最后一位数
    
    Signed-off-by: youngbear <yangshaoxiong2008@163.com>

--- README.md
+++ README.md
@@ -2,6 +2,42 @@
 
 ## [LintCode 官网](http://www.lintcode.com/zh-cn/problem/)
 
+
+### [729. 阶乘除法的最后一位数](http://www.lintcode.com/zh-cn/problem/last-digit-by-factorial-divide/)
+
+**题目描述:**
+
+给出两个数 A 和 B, 其中 B >= A. 我们需要计算结果 F 的最后一位数是什么, 其中F = B! / A!(1 <= A, B <= 10^18, A 和 B 非常大)
+
+**思路：**
+
+根据描述，可以得到：
+
+```
+F = B! / A!，即F = B*(B-1)*(B-2)*...*(A + 1)
+```
+
+主要分为两种情况：
+
+1. 如果B-A >= 10，则表明B和A之间，肯定存在一个数为10的倍数，则这种情况下，末位为0。
+2. 如果B-A < 10，则只需要计算<font color=red>**末位的结果**</font>即可。从A+1一直乘到B，用一个int值保存末位值。
+
+主要代码为：
+
+```
+    public int computeLastDigit(long A, long B) {
+        if (B - A >= 10) {
+            return 0;
+        }
+        int result = 1;
+        for (long i = A + 1; i <= B; i++) {
+            int temp = (int) (i % 10);
+            result = (result * temp) % 10;
+        }
+        return result;
+    }
+```
+
 ### [742. Digit Divide Numbers](http://www.lintcode.com/zh-cn/problem/digit-divide-numbers/)
 
 **题目描述:**
---
+++ src/com/ysx/lintcode/practice2017/medium/Factorial.java
@@ -0,0 +1,36 @@
+package com.ysx.lintcode.practice2017.medium;
+
+/**
+ * http://www.lintcode.com/zh-cn/problem/last-digit-by-factorial-divide/
+ * 729. 阶乘除法的最后一位数
+ * 给出两个数 A 和 B, 其中 B >= A. 我们需要计算结果 F 的最后一位数是什么,
+ * 其中F = B! / A!(1 <= A, B <= 10^18, A 和 B 非常大)
+ */
+public class Factorial {
+
+    public static void main(String[] args) {
+        Factorial test = new Factorial();
+        System.out.println(test.computeLastDigit(1L, 4L));
+    }
+
+    /*
+     * @param A: the given number
+     * @param B: another number
+     * @return: the last digit of B! / A!
+     * 思路：
+     * F = B! / A!，即F = B*(B-1)*(B-2)*...*(A + 1)
+     * 如果B-A >= 10，则表明B和A之间，肯定存在一个数为10的倍数，则这种情况下，末位为0
+     * 如果B-A < 10，则只需要计算末位的结果即可。从A+1一直乘到B，用一个int值保存末位值。
+     */
+    public int computeLastDigit(long A, long B) {
+        if (B - A >= 10) {
+            return 0;
+        }
+        int result = 1;
+        for (long i = A + 1; i <= B; i++) {
+            int temp = (int) (i % 10);
+            result = (result * temp) % 10;
+        }
+        return result;
+    }
+}
