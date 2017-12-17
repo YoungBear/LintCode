68840f91ee44eb01fbc9f10d468bb927e7855623
YoungBear <yangshaoxiong2008@163.com>
2017-12-17 10:50:53 +0800

    add ABCSubString
    Signed-off-by: YoungBear <yangshaoxiong2008@163.com>

---
+++ src/com/ysx/lintcode/practice2017/medium/ABCSubString.java
@@ -0,0 +1,41 @@
+package com.ysx.lintcode.practice2017.medium;
+
+/**
+ * 734. 形式为a^i b^j c^k的子序列数量
+ * http://www.lintcode.com/zh-cn/problem/number-of-subsequences-of-form-ai-bj-ck/
+ */
+public class ABCSubString {
+
+    public static void main(String[] args) {
+        ABCSubString test = new ABCSubString();
+        System.out.println("abbc: " + test.countSubsequences("abbc"));
+        System.out.println("abcabc: " + test.countSubsequences("abcabc"));
+
+    }
+
+    /*
+     * @param : the input string
+     * @return: the number of subsequences
+     */
+    public int countSubsequences(String source) {
+        int aCount = 0;
+        int bCount = 0;
+        int cCount = 0;
+        for (int i = 0; i < source.length(); i++) {
+            switch (source.charAt(i)) {
+                case 'a':
+                    aCount = aCount + aCount + 1;
+                    break;
+                case 'b':
+                    bCount = aCount + bCount + bCount;
+                    break;
+                case 'c':
+                    cCount = bCount + cCount + cCount;
+                    break;
+                default:
+                    break;
+            }
+        }
+        return cCount;
+    }
+}
