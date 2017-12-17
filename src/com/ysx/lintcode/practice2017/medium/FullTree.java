f9e53d27df3050871eb8321b80844cfda3b1d4a7
YoungBear <yangshaoxiong2008@163.com>
2017-12-17 11:36:12 +0800

    add isFullTree
    Signed-off-by: YoungBear <yangshaoxiong2008@163.com>

---
+++ src/com/ysx/lintcode/practice2017/medium/FullTree.java
@@ -0,0 +1,33 @@
+package com.ysx.lintcode.practice2017.medium;
+
+
+/**
+ * 726. 验证满二叉树
+ * http://www.lintcode.com/zh-cn/problem/check-full-binary-tree/
+ */
+public class FullTree {
+
+    /*
+     * @param : the given tree
+     * @return: Whether it is a full tree
+     */
+    public boolean isFullTree(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        if ((root.left != null && root.right == null) ||
+                (root.left == null && root.right != null)) {
+            return false;
+        }
+        return isFullTree(root.left) && isFullTree(root.right);
+    }
+
+    private static final class TreeNode {
+        int value;
+        TreeNode left;
+        TreeNode right;
+
+    }
+
+
+}
