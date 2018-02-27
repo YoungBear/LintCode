329850d6a369a0de63ab85e4464a78f40975cbce
youngbear <yangshaoxiong2008@163.com>
2018-02-27 18:45:35 +0800

    Leetcode 788. Rotated Digits. (only java code)
    https://leetcode.com/problems/rotated-digits/description/
    
    Signed-off-by: youngbear <yangshaoxiong2008@163.com>

---
+++ JavaCode/src/com/ysx/leetcode/easy/RotatedDigits.java
@@ -0,0 +1,41 @@
+package com.ysx.leetcode.easy;
+
+/**
+ * Leetcode 788. Rotated Digits
+ * https://leetcode.com/problems/rotated-digits/description/
+ */
+public class RotatedDigits {
+
+    public static void main(String[] args) {
+        RotatedDigits test = new RotatedDigits();
+        System.out.println(test.rotatedDigits(10));
+
+    }
+
+    public int rotatedDigits(int N) {
+        int sum = 0;
+        for (int i = 1; i <= N; i++) {
+            if (isGoodDigit(i)) {
+                sum++;
+            }
+        }
+        return sum;
+    }
+
+    public static boolean isGoodDigit(int n) {
+        int t = n;
+        int s = 1;
+        boolean flag = false;
+        while (t > 0) {
+            int remainder = t % 10;
+            if (3 == remainder || 4 == remainder || 7 == remainder) {
+                return false;
+            } else if (2 == remainder || 5 == remainder || 6 == remainder || 9 == remainder) {
+                flag = true;
+            }
+            t /= 10;
+        }
+        return flag;
+    }
+
+}
